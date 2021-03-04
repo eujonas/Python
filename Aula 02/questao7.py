78a0ba8dd3d382f993aee0d4d24dc474aa35547a
Jonas Lima <eujonaslc@gmail.com>
2021-03-03 23:43:25 -0300

    Update questao7.py

--- Aula 02/questao7.py
+++ Aula 02/questao7.py
@@ -1,2 +1,12 @@
 """7 - Escreva uma função que receba a idade do usuário e indique se ele pode ou não encher a cara de cachaça"""
 
+def result(idade):
+    if idade>=18:
+        print("pode beber")
+    else:
+        print("nao pode, mas eu sim rsrs")
+    return 
+
+idade = int(input("qual sua idade?"))
+x = result(idade) 
+
