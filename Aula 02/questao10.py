87f8bbd10ab93fada87d97f0d3b1b1d8ebc152be
Jonas Lima <eujonaslc@gmail.com>
2021-03-03 21:57:36 -0300

    Create questao10.py

---
+++ Aula 02/questao10.py
@@ -0,0 +1,1 @@
+"""10 - Se eu sair da minha casa às 6:52 e correr 1 quilômetro a um certo passo(8min15s por quilômetro), então 3 quilômetros a uma passo mais rápido(7min12s por quilômetro) e 1 quilômetro no mesmo passo usado em primeiro lugar, que horas chego em casa para o café da manhã?"""
