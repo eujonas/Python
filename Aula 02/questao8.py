650aa9a26b2c52808d003eaafc6b93055e0424c4
Jonas Lima <eujonaslc@gmail.com>
2021-03-04 13:45:58 -0300

    Update questao8.py

--- Aula 02/questao8.py
+++ Aula 02/questao8.py
@@ -1,1 +1,9 @@
 """8 - P = True e Q = False. Aplique De Morgan na seguinte proposição e atribua o valor a uma variável - ~(p ^ (p v q)), essa variável deve ser retornada partir de uma função"""
+
+def result(P,Q):
+    resposta = not P or(not P and not Q)
+    return resposta
+x = result(True,False) 
+print(x)
+
+
