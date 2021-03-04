be36f1d1ded1c054b56044c2f795e61ae199bd96
Jonas Lima <eujonaslc@gmail.com>
2021-03-04 13:46:09 -0300

    Update questao9.py

--- Aula 02/questao9.py
+++ Aula 02/questao9.py
@@ -1,1 +1,12 @@
 """9 - Crie uma função que receba duas strings e retorne True se o número de elementos de uma for igual ao da outra, e retorne False caso contrário.Pesquise pelo método len() na documentação do Python."""
+
+def result(palavra1,palavra2):
+    if len(palavra1) == len(palavra2):
+        print("verdadeiro")
+    else:
+        print("false")
+    return 
+palavra1 = input("primeira palavra: ")
+palavra2 = input("segunda palavra: ")
+x = result(palavra1,palavra2) 
+
