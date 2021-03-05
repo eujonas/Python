ea753d4392d1c0ebf9a8d560fa117be813e9b043
Jonas Lima <eujonaslc@gmail.com>
2021-03-05 13:15:16 -0300

    Update questao10.py

--- Aula 02/questao10.py
+++ Aula 02/questao10.py
@@ -1,8 +1,12 @@
 """10 - Se eu sair da minha casa às 6:52 e correr 1 quilômetro a um certo passo(8min15s por quilômetro), então 3 quilômetros a uma passo mais rápido(7min12s por quilômetro) e 1 quilômetro no mesmo passo usado em primeiro lugar, que horas chego em casa para o café da manhã?"""
 
-saida = 6.52
-chegada = 0.0815 + 0.0712 + 0.0815 + saida        
 
-print(chegada)
+#total = (15 + 12*3 + 15) + ((52 + 8 + 7*3 + 8) * 60) + (6 * 60 * 60)
+total = 27006 #em segundos
 
-#questao incompleta
+seg = (total % 60) % 60
+minutos = (total // 60) % 60
+horas = (total // 60) // 60
+
+print(f"voce chegara as {horas}:{minutos}:{seg}")
+
