3cfa511ff9bd6a10e6ee53010dd860ca5c2379e9
Jonas Lima <eujonaslc@gmail.com>
2021-03-09 21:24:43 -0300

    Create questao03.py

---
+++ Aula 04/questao03.py
@@ -0,0 +1,3 @@
+"""Escreva um função que receba sua idade, sua profissão e comormidade = False, altere o valor deste último parâmetro na chamada da função, caso necessário. Essa função deve retornar "Hápto" se você estiver dentro do grupo prioritário para receber a primeira dose da vacina."""
+
+
