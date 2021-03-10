7a346459b04db7d1a53a7cd74c356260ba543d00
Jonas Lima <eujonaslc@gmail.com>
2021-03-09 21:25:15 -0300

    Create questao08.py

---
+++ Aula 04/questao08.py
@@ -0,0 +1,5 @@
+"""8 - Escreva as seguintes funções lambda.
+Uma função que receba um nome e retorne o seu equivalente maiúsculo
+Uma função que receba dois parâmetros e retorne o primeiro elevado ao segundo
+Uma função que receba um dado e retorne seu tipo
+A questão 9 e 14 da lista 2"""
